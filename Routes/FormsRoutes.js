0a41ca9125371b8a42941100ce115c47a9d4ff20
ashu7982 <ashutoshdubey7982@gmail.com>
2023-12-08 17:21:05 +0530

    done

--- Routes/FormsRoutes.js
+++ Routes/FormsRoutes.js
@@ -1,72 +1,70 @@
 
 
-const express = require('express')
-const router= express.Router()
-const Form= require('../Models/FormsModel')
+const express = require('express');
+const router = express.Router();
+const Form = require('../Models/FormsModel');
 
-// to get all data
-router.get('/',async (req,res)=>{
+router.get('/', async (req, res) => {
     try {
-        const forms= await Form.find()
-        res.send(forms)
+        const forms = await Form.find();
+        res.send(forms);
     } catch (error) {
-        res.send(error.message)
+        res.send(error.message);
     }
-})
-//to get single form
-router.get('/get/:id',async (req,res)=>{
-    const params = req.params
-    try {
-        const forms= await Form.find({_id: params.id})
-        res.send(forms)
-    } catch (error) {
-        res.send(error.message)
-    }
-})
-//post new form
-router.post('/',async (req,res)=>{
-    const data= req.body
-    if(!data){ res.status(404).send({message:"please pass body"}) }
-    try {
-        const form= await Form.create(data)
-        // form.save()
-        res.send({message:'form created successfully',data:form})
-    } catch (error) {
-        res.send(error.message)
-    }
-})
+});
 
-//delete form
-router.delete('/:id',async (req,res)=>{
-    const id= req.params.id
-    
+router.get('/get/:id', async (req, res) => {
+    const params = req.params;
     try {
-        if(!id){
-            res.status(404).send({message:'id not specified'})    
+        const forms = await Form.find({ _id: params.id });
+        res.send(forms);
+    } catch (error) {
+        res.send(error.message);
+    }
+});
+
+router.post('/', async (req, res) => {
+    const data = req.body;
+    if (!data) {
+        res.status(404).send({ message: "body" });
+    }
+    try {
+        const form = await Form.create(data);
+        res.send({ message: 'created successfully', data: form });
+    } catch (error) {
+        res.send(error.message);
+    }
+});
+
+// Delete form
+router.delete('/:id', async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        if (!id) {
+            res.status(404).send({ message: 'not clear' });
         }
-        const form= await Form.findByIdAndDelete(id)
-        res.send({message:'form deleted successfully',data:form})
+        const form = await Form.findByIdAndDelete(id);
+        res.send({ message: 'deleted successfully', data: form });
     } catch (error) {
-        res.send(error.message)
+        res.send(error.message);
     }
-})
+});
 
-//edit form
-router.patch('/:id',async (req,res)=>{
-    const id= req.params.id
-    const data= req.body
+// Edit form
+router.patch('/:id', async (req, res) => {
+    const id = req.params.id;
+    const data = req.body;
     try {
-        if(!id){
-            res.status(404).send({message:'id not specified'})    
+        if (!id) {
+            res.status(404).send({ message: 'not clear' });
         }
 
-        const form= await Form.findByIdAndUpdate(id,{data})
-        res.send({message:'form updated successfully',data:form})
+        const form = await Form.findByIdAndUpdate(id, { data });
+        res.send({ message: 'updated successfully', data: form });
     } catch (error) {
-        res.send(error.message)
+        res.send(error.message);
     }
-})
+});
 
-
-
-module.exports = router
+module.exports = router;
--- Routes/ResponseRoute.js
+++ Routes/ResponseRoute.js
@@ -1,26 +1,31 @@
 
 
-const express = require('express')
-const router= express.Router()
-const Response= require('../Models/ResponseModel')
+const express = require('express');
+const router = express.Router();
+const Response = require('../Models/ResponseModel');
 
-router.get('/:formId',async (req,res)=>{
-    try {
-        const response= await Response.find({"formId":req.params.formId})
-        res.send({message:"response Fetched successfully",data:response})
-    } catch (error) {
-        res.send(error.message)
-    }
-})
-router.post('/',async (req,res)=>{
-    const data= req.body
-    if(!data){ res.status(404).send({message:"please pass body"}) }
-    try {
-        const response= await Response.create(data)
-        res.send({message:'form created successfully',data:response})
-    } catch (error) {
-        res.send(error.message)
-    }
-})
 
-module.exports = router
+router.get('/:formId', async (req, res) => {
+    try {
+        const response = await Response.find({ "formId": req.params.formId });
+        res.send({ message: "fetched success", data: response });
+    } catch (error) {
+        res.send(error.message);
+    }
+});
+
+
+router.post('/', async (req, res) => {
+    const data = req.body;
+    if (!data) {
+        res.status(404).send({ message: "body" });
+    }
+    try {
+        const response = await Response.create(data);
+        res.send({ message: 'success', data: response });
+    } catch (error) {
+        res.send(error.message);
+    }
+});
+
+module.exports = router;
